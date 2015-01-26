6338cbe1657dc9cf2c0746f32eb16f0d34d9bac0
Derrick <derrick.b.gold@gmail.com>
2015-01-26 12:42:13 -0700

    Fixed redirection on user login

--- app/controllers/SessionsController.php
+++ app/controllers/SessionsController.php
@@ -14,13 +14,15 @@ class SessionsController extends BaseController {
       public function store() {
           
       	 if (Auth::attempt(Input::only('email', 'password'))) {
+             
              $response = array(
                  "status" => 0,
-                 "redirect" => "/users/".Auth::user()->username
+                 "redirect" => URL::route('users.edit', Auth::user()->username)
              );
              
+             
              return Response::json($response);
-	     }
+	   }
           
           $response = array(
                 "status" => 1,
