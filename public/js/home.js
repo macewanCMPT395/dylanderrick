b72ff24589ea829e86a2386a5b3770ea4e117330
Derrick <derrick.b.gold@gmail.com>
2015-01-28 11:20:48 -0700

    -Forgot to push these files

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var myLightBox = LightBox.init();
     
-    $(".titleBlock .loginBlock #loginButton").click(function(e) {
+    $(".titleBlock .menuBlock #loginButton").click(function(e) {
         e.preventDefault();     
         $.get('/login', function(data) {
 
@@ -10,7 +10,7 @@ $(document).ready(function(){
         });      
     });
     
-    $(".titleBlock .loginBlock #registerButton").click(function(e) {
+    $(".titleBlock .menuBlock #registerButton").click(function(e) {
         e.preventDefault();
         $.get('/users/create', function(data) {
             myLightBox.width("400px").height("400px");
